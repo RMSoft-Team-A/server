c4fc4425c51187d958f6c0a02ac5d5a7dabd098b
ulsandonghun <donghoon141@naver.com>
2023-07-17 16:34:36 +0900

    [init] : Setting

---
+++ src/main/java/com/example/RMSoftProject/RmSoftProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.RMSoftProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmSoftProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmSoftProjectApplication.class, args);
+	}
+
+}
