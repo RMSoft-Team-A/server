50865890d59ca40b078736a9b54ab41e724c84b2
ulsandonghun <donghoon141@naver.com>
2023-07-17 16:55:32 +0900

    [add] : Squid 도메인과 User 도메인 추가

---
+++ src/main/java/com/example/RMSoftProject/Domain/Squid/Squid.java
@@ -0,0 +1,28 @@
+package com.example.RMSoftProject.Domain.Squid;
+
+
+import com.example.RMSoftProject.Domain.User.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Squid {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "squid_id")
+
+    private Long id;
+
+    private String Level;
+
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+}
---
+++ src/main/java/com/example/RMSoftProject/Domain/User/User.java
@@ -0,0 +1,28 @@
+package com.example.RMSoftProject.Domain.User;
+
+
+import com.example.RMSoftProject.Domain.Squid.Squid;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String squidname;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Squid squid;
+}
