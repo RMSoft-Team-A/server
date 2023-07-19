ca4753eee630f196c7acfc6d7fcea7868ccdf316
ulsandonghun <donghoon141@naver.com>
2023-07-19 15:59:18 +0900

    [test] : 테스트 코드 추가

---
+++ src/test/java/com/example/RMSoftProject/Domain/User/UserControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.RMSoftProject.Domain.User;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//
+//class UserControllerTest {
+//    UserService userService;
+//    UserRepository userRepository;
+//
+//
+//    @Test
+//    void getUserByEmail() {
+//        User user =
+//                User.builder()
+//                        .email("donghun")
+//                        .password("124").build();
+//        userService;
+//
+//    }
+//
+//    @Test
+//    void login() {
+//    }
+//}
